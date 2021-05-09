6f430253cbcb82276359c94a79be7edbbd09ab5a
gauravbgh <sbgaurav02@gmail.com>
2021-05-09 13:13:26 +0530

    saved

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ model_load = pdt_recommendation()
 
 @app.route('/')
 def home():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
 @app.route("/predict", methods=['POST'])
 def predict():
     if (request.method == 'POST'):
-        username = request.args['USERNAME']
+        username = request.form.get("USERNAME")
         top_5_pdt = model_load.predict(str(username)) 
-        output = "\n1) {0} \n2) {1} \n3) {2} \n4) {3} \n5) {4}".format(top_5_pdt[0],top_5_pdt[1],top_5_pdt[2],top_5_pdt[3],top_5_pdt[4])
-        return render_template('index.html', prediction_text='{top 5 recommendations are : {0}'.format(output))
+
+        return render_template('index.html', prediction_text= top_5_pdt )
     else :
         return render_template('index.html')
 
